0bac59707f1b9775eaa4a25607aafa6af6c708db
WestonSeyler <729518161@qq.com>
2023-07-06 23:15:03 +0800

    feat: inject

--- all-decorator/src/app.controller.ts
+++ all-decorator/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Optional } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  // constructor(private readonly appService: AppService) {}
+  @Inject(AppService)
+  // private readonly appService: AppService;
+  @Optional()
+  @Inject('customer')
+  private readonly customer: Record<string, any>;
+  constructor(@Optional() private appService: AppService) {}
   @Get()
   getHello(): string {
+    console.log(this.customer);
     return this.appService.getHello();
   }
 }
--- all-decorator/src/app.module.ts
+++ all-decorator/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Customer } from './customer';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    Customer,
+    // {
+    //   provide: 'customer',
+    //   useFactory() {
+    //     return {
+    //       name: 'customer',
+    //     };
+    //   },
+    // },
+  ],
 })
 export class AppModule {}
