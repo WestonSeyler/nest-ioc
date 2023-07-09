b42b6d6dddd09976a5f7e22c075aa885b11cd98d
吴佳宁 <jianing.wu@wowjoy.cn>
2023-07-09 22:55:40 +0800

    feat: reflect

---
+++ metadata-and-reflector/src/aaa.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AaaInterceptor implements NestInterceptor {
+  @Inject(Reflector)
+  private reflector: Reflector;
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Interceptor');
+    console.log(this.reflector.get('roles', context.getHandler()));
+
+    return next.handle();
+  }
+}
--- metadata-and-reflector/src/main.ts
+++ metadata-and-reflector/src/main.ts
@@ -5,4 +5,39 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
+function test() {
+  function Type(type) {
+    return Reflect.metadata('design:type', type);
+  }
+  function ParamTypes(...types) {
+    return Reflect.metadata('design:paramtypes', types);
+  }
+  function ReturnType(type) {
+    return Reflect.metadata('design:returntype', type);
+  }
+  @ParamTypes(String, Number)
+  class Test {
+    constructor(text, i) {}
+
+    @Type(String)
+    get name() {
+      return 'text';
+    }
+
+    @Type(Function)
+    @ParamTypes(Number, Number)
+    @ReturnType(Number)
+    add(x, y) {
+      return x + y;
+    }
+  }
+
+  const obj = new Test('test', 1);
+  // Reflect.getMetadata 的 api 取出了 add 方法的参数的类型。
+  const paramtypes = Reflect.getMetadata('design:paramtypes', obj, 'add');
+  console.log(paramtypes);
+}
+test();
+
 bootstrap();
