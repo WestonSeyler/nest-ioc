3a9ded4b768c2dc98fb9524378e140f648955f55
WestonSeyler <729518161@qq.com>
2023-07-10 22:26:12 +0800

    feat: import provider循环依赖

---
+++ module-test/src/ccc.service.ts
@@ -0,0 +1,15 @@
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { DddService } from './ddd.service';
+
+@Injectable()
+export class CccService {
+  constructor(
+    @Inject(forwardRef(() => DddService)) private dddService: DddService,
+  ) {}
+  ccc() {
+    return 'ccc';
+  }
+  ddd() {
+    return this.dddService.ddd();
+  }
+}
