db2a3f1f45973b15b6294be5ac08619c51c05897
WestonSeyler <729518161@qq.com>
2023-07-23 11:19:24 +0800

    feat: pipe

---
+++ pipe-test/src/app.controller.ts
@@ -0,0 +1,82 @@
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseArrayPipe,
+  ParseBoolPipe,
+  ParseEnumPipe,
+  ParseFloatPipe,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+
+enum Ggg {
+  aaa = 'aaa',
+  bbb = 'bbb',
+  ccc = 'ccc',
+}
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  //可以把他转换成整型
+  getHello(
+    @Query(
+      'aaa',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_FOUND,
+      }),
+    )
+    aaa: string,
+  ): string {
+    // return this.appService.getHello();
+    return aaa + 1;
+  }
+
+  @Get('bb')
+  //可以把他转换成浮点型
+  getHello2(
+    @Query('bbb', ParseFloatPipe)
+    bbb: string,
+  ): string {
+    // return this.appService.getHello();
+    return bbb + 1;
+  }
+
+  @Get('cc')
+  //可以把他转换成布尔型
+  getHello3(
+    @Query('cc', ParseBoolPipe)
+    cc: string,
+  ): string {
+    // return this.appService.getHello();
+    return cc + 1;
+  }
+
+  @Get('ee')
+  //可以把他转换成布尔型
+  getHello4(
+    @Query(
+      'ee',
+      new ParseArrayPipe({
+        // items: Number,
+        //指定分隔符
+        separator: ',',
+        optional: true,
+      }),
+    )
+    ee: Array<number>,
+  ) {
+    // return this.appService.getHello();
+    return ee;
+    // return ee.reduce((a, b) => a + b, 0);
+  }
+
+  @Get('gg/:enum')
+  gg(@Param('enum', new ParseEnumPipe(Ggg)) e: Ggg) {
+    return e;
+  }
+}
