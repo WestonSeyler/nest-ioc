0583b13929a9084e9d71de1fd3c15656b901d695
吴佳宁 <jianing.wu@wowjoy.cn>
2023-08-06 16:24:31 +0800

    feat: upload

--- nest-multer-upload/src/app.controller.ts
+++ nest-multer-upload/src/app.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UploadedFile,
+  UseInterceptors,
+  Post,
+  Body,
+  UploadedFiles,
+} from '@nestjs/common';
+import {
+  FileInterceptor,
+  FilesInterceptor,
+  AnyFilesInterceptor,
+} from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +21,44 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('aaa')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    console.log('body', body);
+    console.log('file', file);
+  }
+  //多文件上传
+
+  @Post('bbb')
+  @UseInterceptors(
+    FilesInterceptor('bbb', 3, {
+      dest: 'uploads',
+    }),
+  )
+  uploadFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('files', files);
+    console.log('body', body);
+  }
+  @Post('ddd')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads',
+    }),
+  )
+  uploadAnyFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
   }
 }
