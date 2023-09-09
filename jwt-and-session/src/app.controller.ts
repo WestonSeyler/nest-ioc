bfa558ced8ef7f1340c7c96ae32edfa4f3763709
吴佳宁 <jianing.wu@wowjoy.cn>
2023-09-09 19:58:37 +0800

    feat: token

--- jwt-and-session/src/app.controller.ts
+++ jwt-and-session/src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Session } from '@nestjs/common';
+import { Controller, Get, Inject, Res, Session, Headers } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
 
   @Get()
   getHello(): string {
@@ -14,5 +19,30 @@ export class AppController {
     console.log(session);
     session.count = session.count ? session.count + 1 : 1;
     return session.count;
+  }
+
+  @Get('ttt')
+  ttt(
+    @Headers('authorization') authorization: string,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    if (authorization) {
+      try {
+        const token = authorization.split(' ')[1];
+        const data = this.jwtService.verify(token);
+        const newToken = this.jwtService.sign({
+          code: data.count + 1,
+        });
+        response.setHeader('token', newToken);
+        return data.count + 1;
+      } catch (error) {}
+      // console.log(authorization, '1111');
+    } else {
+      const newToken = this.jwtService.sign({
+        count: 1,
+      });
+      response.setHeader('token', newToken);
+      return 'hello';
+    }
   }
 }
